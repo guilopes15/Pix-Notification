2eac6d68130cfe3f385ecdfaa69a7490f7c8cafe
guilherme <guilherme-lopes58@hotmail.com>
2025-08-08 18:54:47 -0300

    Corrigindo nomenclatura dos testes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,10 @@
 from fastapi.testclient import TestClient
 from src.pix_notification.app import app
 
-
 client = TestClient(app)
 
-def test_app():
+
+def test_send_message():
     response = client.post('/send', json={'message': 'Test'})
 
     assert response.status_code == 200
